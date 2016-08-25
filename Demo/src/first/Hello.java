a6dcf42a5d66677ff62d4fc88df53fed62fc9f5e
sobhan_banerjee <sobhan_banerjee@infosys.com>
2016-08-25 14:36:18 +0530

    Initialcommit

---
+++ Demo/src/first/Hello.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Git Project");
+		System.out.println("");
+
+	}
+
+}
